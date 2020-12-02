a9909e34bb4f1db3a874c538054cb8c72287de7b
charles-42 <charles.beniac@gmail.com>
2020-12-02 10:58:37 +0100

    corrige le bug static file erreur 500

--- timeline_project/settings.py
+++ timeline_project/settings.py
@@ -139,6 +139,7 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
+
 if os.environ.get('ENV') == 'PRODUCTION':
 
     # Static files settings
@@ -155,5 +156,5 @@ if os.environ.get('ENV') == 'PRODUCTION':
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES = {'default': dj_database_url.config()} 
+    DATABASES = {'default': dj_database_url.config()}
     DATABASES.update(default=db_from_env)
